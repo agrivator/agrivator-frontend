6a3dd439bae1d446cb48bdf1fc68633b8a78633b
Bonnie Simon <bonniesimon@protonmail.com>
2020-04-04 14:53:04 +0530

    Added tailwindCSS to the project

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # Agrivator Frontend
 
-## Available Scripts
+## yarn start
+Used to start the app on browser.
+Default opens in port 3000
 
-In the project directory, you can run:
+## including tailwindCSS
+include the main.css file inside src/CSS/tailwind-styles/main.css
 
-### `yarn start`
+## always pull from upstream before pushing
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
 
 
 ## Learn More
--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './CSS/tailwind-styles/main.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto bg-teal-400 shadow-xl h-screen">
+		<h1>Helo world</h1>	
+      </div>
     </div>
   );
 }
