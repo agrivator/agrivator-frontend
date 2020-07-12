c1bfeac03362fff819fb4855f18b64b73052330c
bonnie <bonnie.developer@gmail.com>
2020-07-12 20:50:23 +0530

    Made redirects work if token present😋

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,24 @@
-import React, { Component, useState} from 'react'
+import React, { Component, useState, useEffect} from 'react'
+import {Redirect} from 'react-router-dom';
 import axios from 'axios'
 
 import LoginForm from './components/LoginForm'
 
-import {storeUser, storeToken} from './helpers/authStore';
+import {storeUser, storeToken, getAuthToken} from './helpers/authStore';
 
-const Login = () => {
+const Login = (props) => {
         const [user, setUser] = useState({});
         // * username can be either email or username
         const [username, setUsername] = useState("bonnietest");
         const [password, setPassword] = useState("bonnietest");
+
+
+        useEffect(() => {
+            const token = getAuthToken(); 
+            if(token){
+                props.history.push('/home'); 
+            }
+        }, [])
 
         // *Function to fetch token and user details from strapi
         // @args : username and password
@@ -34,6 +43,8 @@ const Login = () => {
             storeUser(data.user);
             storeToken(data.jwt);
         }
+
+
 
         return (
             <div>
