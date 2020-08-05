be0dffb27071ec36667d9a770820325535bf3a05
bonnie <bonnie.developer@gmail.com>
2020-08-06 01:12:02 +0530

    💢Error two items have same id

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import ViewDataCard from './ViewDataCard';
@@ -6,29 +6,50 @@ import ViewDataCard from './ViewDataCard';
 import { getProducts } from '../helpers/ProductService';
 
 const Products = () => {
-    const [products, setProducts] = useState([]);     
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         (async () => {
             const resData = await getProducts();
-            setProducts(resData);
+            const flatResData = flattenProducts(resData);
+            setProducts(flatResData);
         })();
-        
+
     }, [])
+
+    //* This works
+    // let productsModded = [];
+    // products.forEach(product => {
+    //     product.product_inventories.forEach(product_inventory => {
+    //         console.log(`${product.name} - ${product_inventory.unit} - ${product_inventory.price}`)
+    //         const newProd = {...product, product_inventory};
+    //         productsModded.push(newProd)
+    //     })
+    // }) 
     
-    let productsModded = [];
-    products.forEach(product => {
-        product.product_inventories.forEach(product_inventory => {
-            console.log(`${product.name} - ${product_inventory.unit} - ${product_inventory.price}`)
-            const newProd = {...product, product_inventory};
-            productsModded.push(newProd)
+    
+
+
+    // *Function that flattens product_inventories array
+    // *Creates a new key called product_inventory
+    const flattenProducts = (inputProducts) => {
+        let productsModded = [];
+        inputProducts.forEach(product => {
+            product.product_inventories.forEach(product_inventory => {
+                console.log(`${product.name} - ${product_inventory.unit} - ${product_inventory.price}`)
+                const newProd = { ...product, product_inventory };
+                productsModded.push(newProd)
+            })
         })
-    }) 
-    console.log(productsModded);
+        return productsModded;
+    }
+
+
+
 
     return (
-        <>  
-            {(products.length === 0)? 
+        <>
+            {(products.length === 0) ?
 
                 <div className="text-center">
                     <div class="spinner-border" role="status">
@@ -36,14 +57,14 @@ const Products = () => {
                     </div>
                 </div>
 
-            : 
-                products.map((product)=>{
+                :
+                products.map((product) => {
                     return <ViewDataCard key={product.id} product={product} />
                 })
-            
-            
+
+
             }
-        
+
         </>
     )
 }
