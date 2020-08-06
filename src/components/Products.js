f1fb4a2b20e9b2dec0d21a4af21e16868dc084b9
bonnie <bonnie.developer@gmail.com>
2020-08-06 18:05:58 +0530

    💤Changed some class -> className

--- src/components/Products.js
+++ src/components/Products.js
@@ -53,8 +53,8 @@ const Products = () => {
             {(products.length === 0) ?
 
                 <div className="text-center">
-                    <div class="spinner-border" role="status">
-                        <span class="sr-only">Loading...</span>
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
                     </div>
                 </div>
 
