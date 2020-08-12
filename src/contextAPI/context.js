21d32629c279d9cb0c72060aa2e23db3877ff865
Jyothis P <jyothisp52@gmail.com>
2020-08-13 01:27:46 +0530

    Authenticatio, POST req fixed.

--- src/contextAPI/context.js
+++ src/contextAPI/context.js
@@ -40,20 +40,30 @@ class ProductProvider extends Component {
 
     postProduct = async (product) =>{
 
-        const jwt = localStorage.getItem('token')
+
+        const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMDlmZWY4MjQwOTMzMDAxN2Q4OWQyZSIsImlhdCI6MTU5NzI1NzMyNSwiZXhwIjoxNTk5ODQ5MzI1fQ.ZgQhO2MuuZDAIXrn1qi2oC32It6nwBDJU2FaEQ-3oNI"
+        // const jwt = localStorage.getItem('token')
         const user = localStorage.getItem('user')
         const token = `Bearer ${jwt}`
-        console.log(token)
-        console.log(user)
-        console.log(product)
-        const res = axios.post(`${baseUrl}/products`,{
-            data:product,
-            headers: {
-              'Authorization': token
-            }
-          });
-        
-        console.log(res)
+        console.log("Token:", token)
+        // console.log(user)
+        console.log("Reg data:", product)
+
+        // The error was 
+        // 1. The way we set the Authorization header.
+        // 2. The product object contained some data that wasn't supposed to filled by the user.
+        // 3. axios.post is an async function. So either use await. or use callback functions.
+
+        // 1. During Login time, do this also. Or set this data from localstorage or something. Idk how that works.
+        axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`
+        // 2. During product creation, only send these fields. Other fields, like created_by and all are filled by the server.
+        const res = await axios.post(`${baseUrl}/products`, {
+            name: product['name'],
+            description: product['description'],
+            category: product['category'],
+            subCategory: product['subCategory']
+        });
+        console.log("res:", res)
     }
 
     // =========================================
